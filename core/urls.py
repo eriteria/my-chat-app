3d8c133c2c9362c4646d823831692d1af3ae30f8
Joseph John <joseph.john.ali.2001@gmail.com>
2022-08-16 01:26:00 +0100

    Update

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
+from account import views as account_views
 
 urlpatterns = [
     path('', views.front_page, name='frontpage'),
+    path('login/', account_views.login, name='login'),
 ]
