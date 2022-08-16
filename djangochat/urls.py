0760094edf31c844f81d3056f861d122794d51a3
Joseph John <joseph.john.ali.2001@gmail.com>
2022-08-16 01:44:03 +0100

    Update

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('rooms/', include('room.urls')),
     path('account/', include('account.urls'))
+    path('', include('core.urls')),
 ]
